1d4e788af2a44c254fa9221173293d63b68a7e96
evgeniy-p <mariwield@gmail.com>
2018-07-10 14:42:15 +0300

    test

--- conf.py
+++ conf.py
@@ -6,3 +6,4 @@ class Conf:
     API_PATH = "/test/ver1.0"
     APP_ID = "0b34602057414e2daeddcc8289245c09"  # приложение от имени пользователя "telphin_script"
     APP_SECRET = "5e7aad30eba34b20b0407aeab38b7053"
+test
